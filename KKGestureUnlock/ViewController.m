133957ce96a63997af9844e55a10fb11e987505c
ShanJiJi <752536885@qq.com>
2016-04-15 13:47:27 +0800

    这是第二次提交

--- KKGestureUnlock/ViewController.m
+++ KKGestureUnlock/ViewController.m
@@ -18,11 +18,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    //创建手势界面
     KKGestureView *gestureView = [[KKGestureView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width)];
-    
+    //设置手势界面居中
     gestureView.center = self.view.center;
-    
+    //将手势界面添加到视图中
     [self.view addSubview:gestureView];
     
 }
